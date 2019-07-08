a38bab89e022fab85500e60eaf7a87755fd4dd8f
muhindokiro <blaize1143@gmail.com>
2019-07-08 23:59:34 +0300

    add secretkey,sqlalchemy and email extentions

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    #email configurations
+    #     email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
